9022c853a511bd0083864c66bb3e4bd52d65ada0
maguell <maglutila@gmail.com>
2019-04-09 12:34:45 +0200

    Calculation and adding digits now happens in a function. Used for testing.

--- script.js
+++ script.js
@@ -1,85 +1,85 @@
 const calculator = document.querySelector('.calculator');
+
 const dataset = calculator.dataset;
 dataset.storedNumber = 0;
 dataset.operator = 'none';
 dataset.prevButton
+
 const buttons = document.querySelector('.buttons');
 const display = document.querySelector('.display');
 
-buttons.addEventListener('click', e=> {
+function calculate(operator, number1, number2) {
+    var result = 0;
+    switch (operator) {
+        case 'add':
+            result = Number(number1) + Number(number2);
+            break;
+        case 'subtract':
+            result = Number(number1) - Number(number2);
+            break;
+        case 'multiply':
+            result = Number(number1) * Number(number2);
+            break;
+        case 'divide':
+            result = Number(number1) / Number(number2);
+            break;
+        default:
+            result = number2;
+            break;
+    }
+    return result;
+}
+
+function addDigit(curNumber, value) {
+    var newNumber = value;
+    if (curNumber != 0)
+        newNumber = curNumber + value; 
+
+    return newNumber;
+}
+
+buttons.addEventListener('click', e => {
     var key = e.target;
     var value = key.textContent;
     var func = key.dataset.function;
     var curNumber = display.textContent;
-    
+
     if (key.matches('button')) {
         // Function key
-        if (func)
-        {
-            switch (func)
-            {
+        if (func) {
+            switch (func) {
                 case 'dot':
-                if (curNumber % 1 === 0)/*(dataset.prevButton !== 'dot')*/
-                    display.textContent = curNumber + '.';
-                break;
+                    if (curNumber % 1 === 0) /*(dataset.prevButton !== 'dot')*/
+                        display.textContent = curNumber + '.';
+                    break;
 
                 case 'result':
-                    console.log("Try " + dataset.storedNumber + " and " + dataset.operator);
- 
-                    var result = 0;
-                    switch(dataset.operator)
-                    {
-                        case 'add':
-                        result = Number(dataset.storedNumber) + Number(curNumber);
-                        break;
-                        case 'subtract':
-                        result = Number(dataset.storedNumber) - Number(curNumber);
-                        break;
-                        case 'multiply':
-                        result = Number(dataset.storedNumber) * Number(curNumber);
-                        break;
-                        case 'divide':
-                        result = Number(dataset.storedNumber) / Number(curNumber);
-                        break;
-                        default:
-                        result = curNumber;
-                        break;
-                    }
-                    
-                    console.log("Result of " + dataset.operator + ": "+ dataset.storedNumber + " " + curNumber + " = " + result);
+                    var result = calculate(dataset.operator, dataset.storedNumber, curNumber)
                     display.textContent = result;
                     dataset.storedNumber = 0;
-
-                break;
+                    break;
 
                 case 'clear':
                     display.textContent = 0;
                     dataset.storedNumber = 0;
                     dataset.operator = 'none';
-                break;
+                    break;
 
                 default:
-                    console.log("Operator")
                     dataset.storedNumber = curNumber;
                     display.textContent = 0;
                     dataset.operator = func;
-                break;
+                    break;
             }
 
             dataset.prevButton = func;
         }
 
         // Number key
-        else
-        {
-            if (curNumber != 0)
-                display.textContent = curNumber + value;
-            else
-                display.textContent = value;
+        else {
+            display.textContent = addDigit(curNumber, value);
 
             dataset.prevButton = 'number';
         }
-        
-        console.log("Stock:" + dataset.storedNumber);
     }
-})  
+})
