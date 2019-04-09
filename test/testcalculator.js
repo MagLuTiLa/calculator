3631c029e203afd0dd1da69c8d35699d21e52212
maguell <maglutila@gmail.com>
2019-04-09 12:40:00 +0200

    Added unit test for addition

---
+++ test/testcalculator.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+var calculator = require('./script.js');
+
+
+describe('Calculator unit tests', function() {
+
+    it('Addition', function(done) {
+        assert.equal(calculator.calculate('add', 5, 3), 8);
+        done();
+    });
+});
