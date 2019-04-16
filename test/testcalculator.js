ef1ba903b5a712fd985c2083fb05d6196493e5b6
maguell <maglutila@gmail.com>
2019-04-16 12:32:34 +0200

    Small adjustments to test file

--- test/testcalculator.js
+++ test/testcalculator.js
@@ -5,6 +5,11 @@ var calculator = require('../calculations.js');
 
 describe('Calculator unit tests', function() {
 
+    it('Add digit', function(done) {
+        assert.equal(calculator.addDigit('add', 5, 3), 8);
+        done();
+    });
+
     it('Addition', function(done) {
         assert.equal(calculator.calculate('add', 5, 3), 8);
         done();
