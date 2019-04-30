fa7c8772313f2b8880b97d06f769fa8f53b5ed32
maguell <maglutila@gmail.com>
2019-04-30 02:28:53 +0200

    Created additional unit tests to cover the basics

--- test/testcalculator.js
+++ test/testcalculator.js
@@ -4,14 +4,39 @@ var calculator = require('../calculations.js');
 
 
 describe('Calculator unit tests', function() {
-
-    it('Add digit', function(done) {
-        assert.equal(calculator.addDigit('add', 5, 3), 8);
+    
+    it('Enter the first number', function(done) {
+        assert.equal(calculator.addDigit('0', 5), 5);
         done();
     });
 
-    it('Addition', function(done) {
-        assert.equal(calculator.calculate('add', 5, 3), 8);
+    it('Enter a number after the first', function(done) {
+        assert.equal(calculator.addDigit('5', '3'), 53);
+        done();
+    });
+
+    it('Calculate addition', function(done) {
+        assert.equal(calculator.calculate('add', 5, 3), 5+3);
+        done();
+    });
+
+    it('Calculate substraction', function(done) {
+        assert.equal(calculator.calculate('subtract', 5, 3), 5-3);
+        done();
+    });
+
+    it('Calculate multiplication', function(done) {
+        assert.equal(calculator.calculate('multiply', 5, 3), 5*3);
+        done();
+    });
+
+    it('Calculate division', function(done) {
+        assert.equal(calculator.calculate('divide', 5, 3), 5/3);
+        done();
+    });
+
+    it('Calculate without a valid operator should output the second number', function(done) {
+        assert.equal(calculator.calculate('anythingelsereally', 5, 3), 3);
         done();
     });
 });
